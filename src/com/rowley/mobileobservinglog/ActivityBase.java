3eac1ec5b8d7228249ece011e57195c99d8c9d57
JoeRowley <jrowley2@msn.com>
2011-12-06 22:08:06 -0700

    Custom Menu works on SDK 8+

--- src/com/rowley/mobileobservinglog/ActivityBase.java
+++ src/com/rowley/mobileobservinglog/ActivityBase.java
@@ -1,5 +1,12 @@
 package com.rowley.mobileobservinglog;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -62,77 +69,39 @@ public class ActivityBase extends Activity implements View.OnClickListener{
 	    setMenuBackground(menu);	
 	    return true;
 	}
-	
-	//Used to customize the menu display
-	protected Boolean setMenuBackground(Menu menu){                     
-	    Log.d("JoeDebug", "Enterting setMenuBackGround");
-		getLayoutInflater().setFactory( new Factory() {  
-	        public View onCreateView(String name, Context context, AttributeSet attrs) {
-	            if ( name.equalsIgnoreCase( "com.android.internal.view.menu.IconMenuItemView" ) ) {
-	                try { // Ask our inflater to create the view  
-	                    LayoutInflater f = getLayoutInflater();  
-	                    final View view = f.createView( name, null, attrs );  
-	                    /* The background gets refreshed each time a new item is added the options menu.  
-	                    * So each time Android applies the default background we need to set our own  
-	                    * background. This is done using a thread giving the background change as runnable 
-	                    * object */
-	                    new Handler().post( new Runnable() {  
-	                        public void run () {  
-	                        	Log.d("JoeDebug", "Setting menu background and text color in run()");
-	                            // sets the background color   
-	                            view.setBackgroundResource(R.color.black);
-	                            // sets the text color              
-	                            ((TextView) view).setTextColor(0xAAFF0000);
-	                            // sets the text size              
-	                            ((TextView) view).setTextSize(14);
-	                        }
-	                    } );  
-	                    return view;
-	                }
-	                catch ( InflateException e ) 
-	                {
-	                	if (e != null)
-	                	{
-		                	Log.e("JoeDebug", "JoeExceptionDump: " + e.getMessage());
-		                	Log.e("JoeDebug", "JoeExceptionDump: " + e.getStackTrace().toString());
-	                	}
-	                }
-	                catch ( ClassNotFoundException e ) 
-	                {
-	                	if (e != null)
-	                	{
-		                	Log.e("JoeDebug", "JoeExceptionDump: " + e.getMessage());
-		                	Log.e("JoeDebug", "JoeExceptionDump: " + e.getStackTrace().toString());
-	                	}
-	                }
-	            } 
-	            return null;
-	        }
-	    }); 
-		
-		return super.onCreateOptionsMenu(menu);
-	}
+
 	
 	//the actions taken by each menu item
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Log.d("JoeDebug", "ActivityBase Options Menu Item pressed: " + item.getItemId());
+    	Log.d("JoeDebug", "Content: " + this.getComponentName().toShortString());
 	    // Handle item selection
 	    switch (item.getItemId()) {
 	    case R.id.returnHome:
-	    	//Check whether we are already on home screen
-	    	//If not on home screen then navigate or tear down the activity stack
+	    	//Check whether we are already on home screen. If not on home screen then navigate
+	    	if (!this.getComponentName().toShortString().contains("HomeScreen"))
+	    	{
+	    		Intent settingsIntent = new Intent(this.getApplication(), HomeScreen.class);
+	            startActivity(settingsIntent);
+	    	}
 	        return true;
 	    case R.id.toggleMode:
 	        toggleMode();
 	        return true;
 	    case R.id.settings:
-    		Intent settingsIntent = new Intent(this.getApplication(), SettingsScreen.class);
-            startActivity(settingsIntent);
+	    	if (!this.getComponentName().toShortString().contains("SettingsScreen"))
+	    	{
+	    		Intent settingsIntent = new Intent(this.getApplication(), SettingsScreen.class);
+	            startActivity(settingsIntent);
+	    	}
 	        return true;
 	    case R.id.info:
-    		Intent infoIntent = new Intent(this.getApplication(), InfoScreen.class);
-            startActivity(infoIntent);
+	    	if (!this.getComponentName().toShortString().contains("InfoScreen"))
+	    	{
+	    		Intent infoIntent = new Intent(this.getApplication(), InfoScreen.class);
+	            startActivity(infoIntent);
+		    }
 	        return true;
 	    default:
 	        return super.onOptionsItemSelected(item);
@@ -169,9 +138,289 @@ public class ActivityBase extends Activity implements View.OnClickListener{
 		toggleMode();
 	}
 	
-	//Hook to allow testing of the set menu method
-	public View hookSetMenuBackground()
+	//Used to customize the menu display
+	//hacked from http://stackoverflow.com/questions/2944244/change-the-background-color-of-the-options-menu/5647743#5647743
+	protected Boolean setMenuBackground(Menu menu){                     
+	    Log.d("JoeDebug", "Enterting setMenuBackGround");
+		getLayoutInflater().setFactory( new Factory() {  
+	        public View onCreateView(String name, Context context, AttributeSet attrs) {
+	            if ( name.equalsIgnoreCase( "com.android.internal.view.menu.IconMenuItemView" ) ) {
+	                try { // Ask our inflater to create the view  
+	                    LayoutInflater f = getLayoutInflater();  
+	                    final View[] view = new View[1];
+	                    try
+	                    {
+	                    	view[0] = f.createView( name, null, attrs );  
+	                    }
+	                    catch (InflateException e)
+	                    {
+	                    	hackAndroid23(name, attrs, f, view);
+	                    }
+	                    /* The background gets refreshed each time a new item is added the options menu.  
+	                    * So each time Android applies the default background we need to set our own  
+	                    * background. This is done using a thread giving the background change as runnable 
+	                    * object */
+	                    new Handler().post(new Runnable() {  
+	                        public void run () {  
+	                        	Log.d("JoeDebug", "Setting menu background and text color in run()");
+	                            // sets the background color   
+	                            view[0].setBackgroundResource(R.color.black);
+	                            // sets the text color              
+	                            ((TextView) view[0]).setTextColor(0xAAFF0000);
+	                            // sets the text size              
+	                            ((TextView) view[0]).setTextSize(14);
+	                        }
+	                    } );  
+	                    return view[0];
+	                }
+	                catch ( InflateException e ) 
+	                {
+	                	if (e != null)
+	                	{
+		                	Log.e("JoeDebug", "JoeExceptionDump: " + e.getMessage());
+		                	Log.e("JoeDebug", "JoeExceptionDump: " + e.getStackTrace().toString());
+	                	}
+	                }
+	                catch ( ClassNotFoundException e ) 
+	                {
+	                	if (e != null)
+	                	{
+		                	Log.e("JoeDebug", "JoeExceptionDump: " + e.getMessage());
+		                	Log.e("JoeDebug", "JoeExceptionDump: " + e.getStackTrace().toString());
+	                	}
+	                }
+	            } 
+	            return null;
+	        }
+	    }); 
+		
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	//needed to make the above menu background method work
+	//hacked from http://stackoverflow.com/questions/2944244/change-the-background-color-of-the-options-menu/5647743#5647743
+	static void hackAndroid23(final String name,
+	        final android.util.AttributeSet attrs, final LayoutInflater f,
+	        final View[] view) 
 	{
-		return null;
+	    // mConstructorArgs[0] is only non-null during a running call to inflate()
+	    // so we make a call to inflate() and inside that call our dully XmlPullParser get's called
+	    // and inside that it will work to call "f.createView( name, null, attrs );"!
+	    try 
+	    {
+	    	f.inflate(new XmlPullParser() 
+	    	{
+				public int next() throws XmlPullParserException, IOException 
+				{
+	                try 
+	                {
+	                    view[0] = (TextView) f.createView( name, null, attrs );
+	                } 
+	                catch (InflateException e) 
+	                {
+	                } 
+	                catch (ClassNotFoundException e) 
+	                {
+	                }
+	                throw new XmlPullParserException("exit");
+				}
+
+				//New XmlPullParser require us to implement all abstract methods, so here we go!
+				public void defineEntityReplacementText(String entityName,
+						String replacementText) throws XmlPullParserException {
+					// TODO Auto-generated method stub
+					
+				}
+
+				public int getAttributeCount() {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+
+				public String getAttributeName(int index) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public String getAttributeNamespace(int index) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public String getAttributePrefix(int index) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public String getAttributeType(int index) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public String getAttributeValue(int index) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public String getAttributeValue(String namespace, String name) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public int getColumnNumber() {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+
+				public int getDepth() {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+
+				public int getEventType() throws XmlPullParserException {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+
+				public boolean getFeature(String name) {
+					// TODO Auto-generated method stub
+					return false;
+				}
+
+				public String getInputEncoding() {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public int getLineNumber() {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+
+				public String getName() {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public String getNamespace() {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public String getNamespace(String prefix) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public int getNamespaceCount(int depth)
+						throws XmlPullParserException {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+
+				public String getNamespacePrefix(int pos)
+						throws XmlPullParserException {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public String getNamespaceUri(int pos)
+						throws XmlPullParserException {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public String getPositionDescription() {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public String getPrefix() {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public Object getProperty(String name) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public String getText() {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public char[] getTextCharacters(int[] holderForStartAndLength) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public boolean isAttributeDefault(int index) {
+					// TODO Auto-generated method stub
+					return false;
+				}
+
+				public boolean isEmptyElementTag()
+						throws XmlPullParserException {
+					// TODO Auto-generated method stub
+					return false;
+				}
+
+				public boolean isWhitespace() throws XmlPullParserException {
+					// TODO Auto-generated method stub
+					return false;
+				}
+
+				public int nextTag() throws XmlPullParserException, IOException {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+
+				public String nextText() throws XmlPullParserException,
+						IOException {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				public int nextToken() throws XmlPullParserException,
+						IOException {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+
+				public void require(int type, String namespace, String name)
+						throws XmlPullParserException, IOException {
+					// TODO Auto-generated method stub
+					
+				}
+
+				public void setFeature(String name, boolean state)
+						throws XmlPullParserException {
+					// TODO Auto-generated method stub
+					
+				}
+
+				public void setInput(Reader in) throws XmlPullParserException {
+					// TODO Auto-generated method stub
+					
+				}
+
+				public void setInput(InputStream inputStream,
+						String inputEncoding) throws XmlPullParserException {
+					// TODO Auto-generated method stub
+					
+				}
+
+				public void setProperty(String name, Object value)
+						throws XmlPullParserException {
+					// TODO Auto-generated method stub
+					
+				}
+	    	}, null, false);
+	    } 
+	    catch (InflateException e1) 
+	    {
+	        // "exit" ignored
+	    }
 	}
 }
