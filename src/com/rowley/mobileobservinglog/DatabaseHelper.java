71357ca43c6a96d1d0dee7e0a6781c51cb3851dd
JoeRowley <jrowley2@msn.com>
2012-01-19 22:42:43 -0700

    Got database onCreate and test completed. Needs some fine-tuning, but works for the most part

--- src/com/rowley/mobileobservinglog/DatabaseHelper.java
+++ src/com/rowley/mobileobservinglog/DatabaseHelper.java
@@ -1,9 +1,13 @@
 package com.rowley.mobileobservinglog;
 
+import java.util.UnknownFormatConversionException;
+
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Helper class to handle all database interaction for the Mobile Observing Log app
@@ -17,17 +21,50 @@ public class DatabaseHelper extends SQLiteOpenHelper
 	private static final int VERSION = 1;
 	Context mContext;
 	  
-	public DatabaseHelper(Context context, CursorFactory factory) 
+	public DatabaseHelper(Context context) 
 	{
-		super(context, DATABASE_NAME, factory, VERSION);
+		super(context, DATABASE_NAME, null, VERSION);
 		mContext = context;
+		Log.d("JoeDebug", "DatabaseHelper constructor finish");
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) 
 	{
-		// TODO Auto-generated method stub
+		Log.d("JoeDebug", "DatabaseHelper onCreate called from" + mContext.toString());
 		
+		//create tables
+		db.beginTransaction();
+		try {
+			createTables(db);
+			db.setTransactionSuccessful();
+		} catch (SQLException e) {
+            Log.e("Error creating tables and debug data", e.toString());
+        } finally {
+        	db.endTransaction();
+        }
+		
+		//populate tables
+		db.beginTransaction();
+		try
+		{
+			populatePersistentSettings(db);
+			populateAvailableCatalogs(db);
+			populateObjects(db);
+			db.setTransactionSuccessful();
+		}
+		catch (SQLException e)
+		{
+			Log.e("Error populating tables", e.toString());
+		}
+		catch (Exception e)
+		{
+			Log.e("Error populating tables", e.toString());
+		}
+		finally
+		{
+			db.endTransaction();
+		}
 	}
 
 	@Override
@@ -35,6 +72,138 @@ public class DatabaseHelper extends SQLiteOpenHelper
 	{
 		// TODO Auto-generated method stub
 		
+	}
+
+	/**
+	 * Method used to initially create the tables for the database
+	 */
+	private void createTables(SQLiteDatabase db)
+	{
+		db.execSQL(mContext.getString(R.string.create_settings_table));
+		db.execSQL(mContext.getString(R.string.create_personal_info_table));
+		db.execSQL(mContext.getString(R.string.create_objects_table));
+		db.execSQL(mContext.getString(R.string.create_locations_table));
+		db.execSQL(mContext.getString(R.string.create_telescope_table));
+		db.execSQL(mContext.getString(R.string.create_eyepiece_table));
+		db.execSQL(mContext.getString(R.string.create_target_lists_table));
+		db.execSQL(mContext.getString(R.string.create_target_list_items_table));
+		db.execSQL(mContext.getString(R.string.create_available_catalogs_table));
+	}
+
+	/**
+	 * Method used to seed values in the available catalogs table
+	 * @throws Exception If the number of values don't match the number of columns in the table, we will throw
+	 */
+	private void populateAvailableCatalogs(SQLiteDatabase db) throws Exception
+	{
+		Log.d("JoeDebug", "Starting populateAvailableCatalogs");
+		String sqlStatement;
+		
+		//First, get the values and parse them into individual lines, then parse the values, 
+		//create the sql statement, then execute it
+		String[] settingsLines = parseResourceByLine(R.string.available_catalogs_default_values);
+		
+		for (int i = 0; i < settingsLines.length; i++)
+		{
+			String[] rowData = parseResourceByDelimiter(settingsLines[i]);
+			
+			//There are four columns to fill in this table
+			if (rowData.length == 4)
+			{
+				sqlStatement = String.format("INSERT INTO availableCatalogs (catalogName, installed, numberOfObjects, description) " + 
+						"VALUES ('%s', '%s', '%s', '%s')", rowData[0], rowData[1], rowData[2], rowData[3]);
+				Log.d("JoeDebug", sqlStatement);
+				db.execSQL(sqlStatement);
+			}
+			else
+			{
+				Log.d("JoeDebug", "Error in populateAvailableCatalogs");
+				throw new Exception("There were not 4 values to populate the settings table with. Values were" + rowData.toString());
+			}
+		}
+	}
+
+	/**
+	 * Method used to seed values in the object table
+	 * @throws Exception If the number of values don't match the number of columns in the table, we will throw
+	 */
+	private void populateObjects(SQLiteDatabase db) throws Exception
+	{
+		Log.d("JoeDebug", "Starting populateObjects");
+		String sqlStatement;
+		
+		//I think for mitigation of file size, we should maintain each catalog in an independent resource file. 
+		//So we need to create a string array to hold each of the catalogs that we want to populate and itterate
+		//through that.
+		int[] availableCatalogs = {R.string.messier_catalog_default_values};
+		for (int i = 0; i < availableCatalogs.length; i++)
+		{
+			//First, get the values and parse them into individual lines, then parse the values, 
+			//create the sql statement, then execute it
+			String[] settingsLines = parseResourceByLine(availableCatalogs[i]);
+			
+			for (int j = 0; j < settingsLines.length; j++)
+			{
+				String[] rowData = parseResourceByDelimiter(settingsLines[j]);
+				
+				//There are three columns to fill in this table
+				if (rowData.length == 26)
+				{
+					Log.d("JoeDebug", "In Loop. rowData.length passed the test");
+					
+					for (int k = 0; k < rowData.length; k++)
+					{
+						Log.d("JoeDebug", "rowData[" + k + "]: " + String.format("'%s'", rowData[k]));
+					}
+					
+					sqlStatement = String.format("INSERT INTO objects (designation, commonName, type, magnitude, size," +
+							" distance, constellation, season, rightAscension, declination, catalog, otherCatalogs," +
+							" imageResource, nightImageResource) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s'," +
+							" '%s', '%s', '%s', '%s', '%s', '%s', '%s')", rowData[1], rowData[2], rowData[3], rowData[4], 
+							rowData[5], rowData[6], rowData[7], rowData[8], rowData[9], rowData[10], rowData[11], 
+							rowData[12], rowData[13], rowData[14]);
+					Log.d("JoeDebug", sqlStatement);
+					db.execSQL(sqlStatement);
+				}
+				else
+				{
+					Log.d("JoeDebug", "Error in populateObjects");
+					throw new Exception("There were not 26 values to populate the settings table with. Values were" + rowData.toString());
+				}
+			}
+		}
+	}
+
+	/**
+	 * Method used to seed data in the settings table
+	 * @throws Exception If the number of values don't match the number of columns in the table, we will throw
+	 */
+	private void populatePersistentSettings(SQLiteDatabase db) throws Exception
+	{
+		Log.d("JoeDebug", "Starting populatePersistentSettings");
+		String sqlStatement;
+		
+		//First, get the values and parse them into individual lines, then parse the values, 
+		//create the sql statement, then execute it
+		String[] settingsLines = parseResourceByLine(R.string.settings_default_values);
+		
+		for (int i = 0; i < settingsLines.length; i++)
+		{
+			String[] rowData = parseResourceByDelimiter(settingsLines[i]);
+			
+			//There are three columns to fill in this table
+			if (rowData.length == 3)
+			{
+				sqlStatement = String.format("INSERT INTO settings (settingName, settingValue, visible) VALUES ('%s', '%s', '%d')", rowData[0], rowData[1], Integer.parseInt(rowData[2]));
+				Log.d("JoeDebug", sqlStatement);
+				db.execSQL(sqlStatement);
+			}
+			else
+			{
+				Log.d("JoeDebug", "Error in populatePersistentSettings");
+				throw new Exception("There were not 3 values to populate the settings table with. Values were" + rowData.toString());
+			}
+		}
 	}
 	
 	/**
@@ -50,7 +219,7 @@ public class DatabaseHelper extends SQLiteOpenHelper
 		String[] retVal;
 		
 		String fullText = mContext.getString(resource);
-		retVal = fullText.split("\\n ");
+		retVal = fullText.split("\n");
 		
 		return retVal;
 	}
@@ -62,14 +231,9 @@ public class DatabaseHelper extends SQLiteOpenHelper
 	 * @param resource The String[] that was returned to us from parseResourceByLine
 	 * @return String[][]: Each index represents one line from the resource. Each index then contains a String[] that contains the individual values from that line
 	 */
-	public String[][] parseResourceByDelimiter(String[] resource)
+	public String[] parseResourceByDelimiter(String resource)
 	{
-		String[][] retVal = new String[resource.length][];
-		
-		for (int i = 0; i < resource.length; i++)
-		{
-			retVal[i] = resource[i].split(";");
-		}
+		String[] retVal = resource.split(";", -1); //-1 on the limit so the return value will include all empty strings including one after the final delimiter
 		
 		return retVal;
 	}
--- tests/src/com/rowley/mobileobservinglog/tests/DatabaseHelperTest.java
+++ tests/src/com/rowley/mobileobservinglog/tests/DatabaseHelperTest.java
@@ -4,7 +4,10 @@
 package com.rowley.mobileobservinglog.tests;
 
 import android.app.Instrumentation;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.test.SingleLaunchActivityTestCase;
+import android.util.Log;
 
 import com.rowley.mobileobservinglog.DatabaseHelper;
 import com.rowley.mobileobservinglog.HomeScreen;
@@ -36,7 +39,8 @@ public class DatabaseHelperTest extends SingleLaunchActivityTestCase<HomeScreen>
 		super.setUp();
 		mAut = getActivity();
 		mInstrumentation = getInstrumentation();
-		mCut = new DatabaseHelper(mAut, null);
+		Log.d("JoeDebug", "Helper Test Constructor. About to call new DatabaseHelper");
+		mCut = new DatabaseHelper(mAut);
 	}
 
 	/**
@@ -50,16 +54,97 @@ public class DatabaseHelperTest extends SingleLaunchActivityTestCase<HomeScreen>
 	}
 
 	/**
+	 * Test method for {@link com.rowley.mobileobservinglog.DatabaseHelper#onCreate(SQLiteDatabase db)}.
+	 * 
+	 * We need to call onCreate() on the database and check that the tables were created and spot check some 
+	 * of the seeded values
+	 */
+	public void testOnCreate() 
+	{
+		Log.d("JoeDebug", "testOnCreate");
+		
+		//First drop the tables so we can start fresh (Only do this on an emulator where you don't have data that you want to save)
+		SQLiteDatabase db = mCut.getWritableDatabase();
+		db.execSQL("DROP TABLE IF EXISTS settings;");
+		db.execSQL("DROP TABLE IF EXISTS personalInfo;");
+		db.execSQL("DROP TABLE IF EXISTS objects;");
+		db.execSQL("DROP TABLE IF EXISTS locations;");
+		db.execSQL("DROP TABLE IF EXISTS telescopes;");
+		db.execSQL("DROP TABLE IF EXISTS eyepieces;");
+		db.execSQL("DROP TABLE IF EXISTS targetLists;");
+		db.execSQL("DROP TABLE IF EXISTS targetListItems;");
+		db.execSQL("DROP TABLE IF EXISTS availableCatalogs;");
+		
+		mCut.onCreate(db);
+		
+		//check settings table
+		Cursor dbCursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = 'settings'", null);
+		assertTrue("The settings table was not present", dbCursor.getCount() > 0);
+		
+		//check personal info table
+		dbCursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = 'personalInfo'", null);
+		assertTrue("The personalInfo table was not present", dbCursor.getCount() > 0);
+		
+		//check objects table
+		dbCursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = 'objects'", null);
+		assertTrue("The objects table was not present", dbCursor.getCount() > 0);
+		
+		//check location table
+		dbCursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = 'locations'", null);
+		assertTrue("The locations table was not present", dbCursor.getCount() > 0);
+		
+		//check telescopes table
+		dbCursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = 'telescopes'", null);
+		assertTrue("The telescopes table was not present", dbCursor.getCount() > 0);
+		
+		//check eyepieces table
+		dbCursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = 'eyepieces'", null);
+		assertTrue("The eyepieces table was not present", dbCursor.getCount() > 0);
+		
+		//check target lists table
+		dbCursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = 'targetLists'", null);
+		assertTrue("The targetLists table was not present", dbCursor.getCount() > 0);
+		
+		//check target list items table
+		dbCursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = 'targetListItems'", null);
+		assertTrue("The targetListItems table was not present", dbCursor.getCount() > 0);
+		
+		//check available catalogs table
+		dbCursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = 'availableCatalogs'", null);
+		assertTrue("The availableCatalogs table was not present", dbCursor.getCount() > 0);
+		
+		//check data in available catalogs
+		dbCursor = db.rawQuery("select numberOfObjects from availableCatalogs where catalogName = 'Messier Catalog'", null);
+		dbCursor.moveToFirst();
+		assertEquals("The data in the availableCatalogs table did not match", 110, dbCursor.getInt(0));
+		
+		//check data in settings
+		dbCursor = db.rawQuery("select settingValue from settings where settingName = 'Night Mode Backlight Intensity'", null);
+		dbCursor.moveToFirst();
+		assertEquals("The data in the settings table did not match", "5", dbCursor.getString(0));
+		
+		//check data in objects
+		dbCursor = db.rawQuery("select type, size, declination from objects where designation = 'M1'", null);
+		dbCursor.moveToFirst();
+		assertEquals("The data in the objects table did not match", "Supernova Remnant", dbCursor.getString(0));
+		assertEquals("The data in the objects table did not match", "420\" × 290\"", dbCursor.getString(1));
+		assertEquals("The data in the objects table did not match", "+22° 00\' 52.1\"", dbCursor.getString(2));
+		
+		db.close();
+	}
+
+	/**
 	 * Test method for {@link com.rowley.mobileobservinglog.DatabaseHelper#parseResourceByLine(int resource)}.
 	 * 
 	 * We will send the method a resource with multiple lines, parse it and check the array that is returned
 	 */
 	public void testParseResourceByLine() 
 	{
+		Log.d("JoeDebug", "testParseResourceByLine");
 		String[] lines = mCut.parseResourceByLine(R.string.settings_default_values);
 		assertEquals("Wrong number of lines", 7, lines.length);
 		assertEquals("Wrong string in the first place", "Night Mode Backlight Intensity;5;1", lines[0]);
-		assertEquals("Wrong string in the last place", "Information/About;;1", lines[6]);
+		assertEquals("Wrong string in the last place", "Information/About;NULL;1", lines[6]);
 	}
 
 	/**
@@ -69,24 +154,24 @@ public class DatabaseHelperTest extends SingleLaunchActivityTestCase<HomeScreen>
 	 */
 	public void testParseResourceByDelimiter() 
 	{
-		String[] lines = new String[]{"Night Mode Backlight Intensity;5;true",
-				"Use Device GPS;Yes;false",
-				"Search/Filter Type;Simple;true",
-				"Manage Equipment;;true",
-				"Manage Observing Locations;;true",
-				"Personal Information;;true",
-				"Information/About;;true" };
+		Log.d("JoeDebug", "testParseResourceByDelimiter");
+		String lines = "Night Mode Backlight Intensity;5;1";
 		
-		String[][] parsedValues = mCut.parseResourceByDelimiter(lines);
+		String[] parsedValues = mCut.parseResourceByDelimiter(lines);
 		
-		assertEquals("Wrong number of lines", 7, parsedValues.length);
-		assertEquals("Wrong number of entries in the first line", 3, parsedValues[0].length);
-		assertEquals("Wrong number of entries in the fourth line", 3, parsedValues[3].length);
-		assertEquals("Wrong string in [0][0]", "Night Mode Backlight Intensity", parsedValues[0][0]);
-		assertEquals("Wrong string in [0][1]", "5", parsedValues[0][1]);
-		assertEquals("Wrong string in [0][2]", "true", parsedValues[0][2]);
-		assertEquals("Wrong string in [3][0]", "Manage Equipment", parsedValues[3][0]);
-		assertEquals("Wrong string in [3][1]", "", parsedValues[3][1]);
-		assertEquals("Wrong string in [3][2]", "true", parsedValues[3][2]);
+		assertEquals("Wrong number of entries", 3, parsedValues.length);
+		assertEquals("Wrong string in [0]", "Night Mode Backlight Intensity", parsedValues[0]);
+		assertEquals("Wrong string in [1]", "5", parsedValues[1]);
+		assertEquals("Wrong string in [2]", "1", parsedValues[2]);
+		
+		//test handling of empty columns
+		lines = "Night Mode Backlight Intensity;;";
+		
+		parsedValues = mCut.parseResourceByDelimiter(lines);
+		
+		assertEquals("Wrong number of entries", 3, parsedValues.length);
+		assertEquals("Wrong string in [0]", "Night Mode Backlight Intensity", parsedValues[0]);
+		assertEquals("Wrong string in [1]", "", parsedValues[1]);
+		assertEquals("Wrong string in [2]", "", parsedValues[2]);
 	}
 }
