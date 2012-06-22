08ed97551cbd55c4c75a3a965e4586f4ac687613
Joe Rowley <joe.rowley@bodybuilding.com>
2012-06-21 23:23:56 -0600

    Adding License. Info/About Screen

--- src/com/mobileobservinglog/InfoScreen.java
+++ src/com/mobileobservinglog/InfoScreen.java
@@ -1,15 +1,28 @@
+/*
+ * Copyright (c) 2012 Joe Rowley
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.mobileobservinglog;
 
 import com.mobileobservinglog.R;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class InfoScreen extends ActivityBase{
 
 	//gather resources
-	LinearLayout body;
+	RelativeLayout body;
+	Button closeButton;
 	
 	@Override
     public void onCreate(Bundle icicle) {
@@ -20,7 +33,7 @@ public class InfoScreen extends ActivityBase{
         
         //setup the layout
         setContentView(settingsRef.getInfoLayout());
-        body = (LinearLayout)findViewById(R.id.info_root); 
+        body = (RelativeLayout)findViewById(R.id.info_root); 
 	}
 	
 	@Override
@@ -46,6 +59,18 @@ public class InfoScreen extends ActivityBase{
 	public void setLayout(){
 		setContentView(settingsRef.getInfoLayout());
 		super.setLayout();
+		findButtonSetListener();
 		body.postInvalidate();
 	}
+	
+	private void findButtonSetListener(){
+		closeButton = (Button)findViewById(R.id.about_info_close_button);
+		closeButton.setOnClickListener(closeScreen);
+	}
+	
+	protected final Button.OnClickListener closeScreen = new Button.OnClickListener(){
+    	public void onClick(View view){
+        	onBackPressed();
+    	}
+    };
 }
