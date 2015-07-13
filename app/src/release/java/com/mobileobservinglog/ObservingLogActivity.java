7322ea0de6bc6f996b69993c6c59c8e3363d5677
Joe Rowley <jrowley2@gmail.com>
2015-07-12 20:27:23 -0600

    Updating the Fabric Plugin

--- app/src/release/java/com/mobileobservinglog/ObservingLogActivity.java
+++ app/src/release/java/com/mobileobservinglog/ObservingLogActivity.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 
 import com.crashlytics.android.Crashlytics;
 
+import io.fabric.sdk.android.Fabric;
+
 /**
  * Created by joe on 5/31/15.
  */
 public class ObservingLogActivity extends ObservingLogActivityParent {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Crashlytics.start(this);
+        Fabric.with(this, new Crashlytics());
         super.onCreate(savedInstanceState);
     }
 }
